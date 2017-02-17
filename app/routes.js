9b1b8e2286eb8fead83c6addb5638240b43d8ad3
Troy Giunipero <giunipero@gmail.com>
2017-02-17 13:58:56 +0100

    Rename containers: "xxxPage" => "xxx"

--- app/routes.js
+++ app/routes.js
@@ -2,17 +2,17 @@
 import React from 'react';
 import { Route } from 'react-router';
 import App from './containers/App';
-import LoginPage from './containers/LoginPage';
-import SurveyPage from './containers/SurveyPage';
-import FeedbackPage from './containers/FeedbackPage';
-import UserProfilePage from './containers/UserProfilePage';
+import Login from './containers/Login';
+import Survey from './containers/Survey';
+import Feedback from './containers/Feedback';
+import UserProfile from './containers/UserProfile';
 
 
 export default (
   <Route path="/" component={App}>
-    <Route path="/survey" component={SurveyPage} />
-    <Route path="/feedback" component={FeedbackPage} />
-    <Route path="/user-profile" component={UserProfilePage} />
-    <Route path="/login" component={LoginPage} />
+    <Route path="/survey" component={Survey} />
+    <Route path="/feedback" component={Feedback} />
+    <Route path="/user-profile" component={UserProfile} />
+    <Route path="/login" component={Login} />
   </Route>
 );
